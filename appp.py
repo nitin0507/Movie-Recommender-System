2ab1c3d6d4760c7432dec8213f1224eaecc199e1
Nitin <83769140+nitin0507@users.noreply.github.com>
2022-09-06 15:13:58 +0530

    Update appp.py

--- appp.py
+++ appp.py
@@ -7,7 +7,7 @@ def fetch_poster(movie_id):
     data = requests.get(url)
     data = data.json()
     poster_path = data['poster_path']
-    full_path = /" + poster_path
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
     return full_path
 
 def recommend(movie):
